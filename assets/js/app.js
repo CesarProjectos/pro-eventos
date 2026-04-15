0159e7f5f0510771ac658f4b760d19cbb8f6fe8d
CesarProjectos <silva.88.cesar@gmail.com>
2026-04-15 18:10:26 +0000

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,15 @@
 console.log("Pro-Eventos online");
+const heroVideo = document.getElementById("heroVideo");
+const year = document.getElementById("year");
+
+if (year) {
+  year.textContent = new Date().getFullYear();
+}
+
+if (heroVideo) {
+  heroVideo.loop = false;
+
+  heroVideo.addEventListener("ended", () => {
+    heroVideo.pause();
+  });
+}
